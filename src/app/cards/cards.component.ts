1941556f56badaba140c356e299534bea1194349
natalia <lomonat@gmail.com>
2018-05-20 16:49:20 +0200

    comments+clean up

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -3,6 +3,12 @@
 *  licensed under the CC0 Creative Commons
 *
 *  The shuffle function is implemented with help of custom pipe in custom-shuffle element (Fisher-Yates shuffle algorithm)
+*  Card meatching mechanism is suitable for large datasets, because there will be build a array once at the initializing step
+*  In this array there will be the objects, and index of object in array will match the ID of object (with -1 step)
+*  the ID will be stored as id of img in template and by clicking on the element the id will be passed to the function and after that
+*  the searched card will be accessed on the position ID-1 -> no looping through an array!!!
+*  Using the shuffle not as function, but as pipe ( the data will be not mutated, but only its view (represantation in the view)
+*  helps to preserve the order of objects in the array
 */
 
 import { Component, OnInit, AfterContentInit, ChangeDetectionStrategy} from '@angular/core';
@@ -25,8 +31,7 @@ export class CardsComponent implements OnInit, AfterContentInit {
     // By initialization the method will be called twice (every card has a pair)
     //for purpose to have a different IDs and pick the right images the method will be called with different params
     //the method builds array of objects for using it in template
-    //also an Object of objects will be build here - for quick access by checking the matching pairs
-    //and setting the properties of objects
+
   this.retrieveImg(1,8,0);
   this.retrieveImg(this.arrayOfObject.length+1,this.arrayOfObject.length*2,this.arrayOfObject.length);
   }
@@ -39,16 +44,15 @@ export class CardsComponent implements OnInit, AfterContentInit {
         'opened': false,
         'id': i
       };
-
-      this.newTmp[i] = tmp;
       this.arrayOfObject.push(tmp);
     }
   }
 
 
   revealCard(event) {
+
     //check if the card wasn't opened
-    if(!(this.newTmp[event.target.id].opened)) {
+    if(!(this.arrayOfObject[(event.target.id-1)].opened)) {
       //allow to open cards and store the IDs before the amount exceed 2
 
       if (this.matchingPairs.length < 2) {
@@ -62,11 +66,11 @@ export class CardsComponent implements OnInit, AfterContentInit {
 
           //check the url's
           //if the cards are the same - clean up matchingArray
-          if (this.newTmp[this.matchingPairs[0]].url == this.newTmp[this.matchingPairs[1]].url ) {
+          if (this.arrayOfObject[(this.matchingPairs[0]-1)].url == this.arrayOfObject[(this.matchingPairs[1]-1)].url ) {
             console.log("found", this.matchingPairs);
             this.matchingPairs = [];
 
-            //if the pair doesn' match - set property "opened" of each card back to false
+            //if the pair doesn't match - set property "opened" of each of 2 cards card back to false
             //clean up matchingArray
             //do this after 2 seconds
           } else {
