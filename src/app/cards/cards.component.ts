4fe56f85a4628bf1d0e92b9901a83827022d6b43
natalia <lomonat@gmail.com>
2018-05-20 16:30:50 +0200

    comments

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,13 +1,14 @@
 /*
 *  the images are token from https://www.pexels.com/royalty-free-images/
 *  licensed under the CC0 Creative Commons
+*
+*  The shuffle function is implemented with help of custom pipe in custom-shuffle element (Fisher-Yates shuffle algorithm)
 */
 
 import { Component, OnInit, AfterContentInit, ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
-  // https://angular-2-training-book.rangle.io/handout/change-detection/change_detection_strategy_onpush.html
-  // cgeck new raference to data
+
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
@@ -16,18 +17,22 @@ export class CardsComponent implements OnInit, AfterContentInit {
 
   public matchingPairs = [];
   public newTmp = {};
- public arrayOfObject = [];
- public coverImg = 'assets/img/cover.jpg';
+  public arrayOfObject = [];
+  public coverImg = 'assets/img/cover.jpg';
 
 
   ngOnInit() {
+    // By initialization the method will be called twice (every card has a pair)
+    //for purpose to have a different IDs and pick the right images the method will be called with different params
+    //the method builds array of objects for using it in template
+    //also an Object of objects will be build here - for quick access by checking the matching pairs
+    //and setting the properties of objects
   this.retrieveImg(1,8,0);
   this.retrieveImg(this.arrayOfObject.length+1,this.arrayOfObject.length*2,this.arrayOfObject.length);
   }
-  // make an OBject with properties to display and identify the images
+  // make an Object with properties to display and identify the images
   public retrieveImg(index, length, helper) {
   let tmp = {};
-
     for (let i = index; i <= length; i++) {
       tmp = {
         'url': 'assets/img/' + (i-helper) + '.jpg',
@@ -42,24 +47,33 @@ export class CardsComponent implements OnInit, AfterContentInit {
 
 
   revealCard(event) {
+    //check if the card wasn't opened
     if(!(this.newTmp[event.target.id].opened)) {
+      //allow to open cards and store the IDs before the amount exceed 2
+
       if (this.matchingPairs.length < 2) {
         this.matchingPairs.push(event.target.id);
-        console.log("push more", this.matchingPairs);
+
+        // if revealed - change "opened" property to true
         this.arrayOfObject[(event.target.id-1)].opened = true;
-        console.log(this.arrayOfObject);
-        console.log(this.newTmp);
+
+        // if 2 cards are picked
         if (this.matchingPairs.length == 2) {
+
+          //check the url's
+          //if the cards are the same - clean up matchingArray
           if (this.newTmp[this.matchingPairs[0]].url == this.newTmp[this.matchingPairs[1]].url ) {
             console.log("found", this.matchingPairs);
             this.matchingPairs = [];
+
+            //if the pair doesn' match - set property "opened" of each card back to false
+            //clean up matchingArray
+            //do this after 2 seconds
           } else {
             setTimeout(()=>{
-              console.log(this.arrayOfObject);
               for (let i = 0; i < this.matchingPairs.length; i ++ ) {
                   this.arrayOfObject[(this.matchingPairs[i]-1)].opened = false;
               }
-              console.log(this.arrayOfObject);
               this.matchingPairs = [];
 
             }, 2000);
@@ -67,6 +81,5 @@ export class CardsComponent implements OnInit, AfterContentInit {
         }
       }
     }
-    return this.newTmp
   }
 }
